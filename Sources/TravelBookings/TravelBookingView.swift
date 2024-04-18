82e8d3e799da0ad7c07a13d204b846369808996e
marcprux <mwp1@cornell.edu>
2024-04-18 13:54:48 -0400

    Refactor cities and bookings view

--- Sources/TravelBookings/TravelBookingView.swift
+++ Sources/TravelBookings/TravelBookingView.swift
@@ -7,53 +7,61 @@ struct TravelBookingView: View {
     @State var departure = Date()
 
     var body: some View {
-        VStack(spacing: 0.0) {
-            ZStack(alignment: .bottom) {
-                AsyncImage(url: city.imageURL) { image in
-                    image.resizable().aspectRatio(contentMode: .fill).frame(height: 200).clipped()
-                } placeholder: {
-                    ProgressView()
+        NavigationStack {
+            VStack(spacing: 0.0) {
+                ZStack(alignment: .bottom) {
+                    AsyncImage(url: city.imageURL) { image in
+                        image.resizable().aspectRatio(contentMode: .fill).frame(height: 200).clipped()
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    Rectangle()
+                        .fill(.linearGradient(colors: [Color.clear, Color.black.opacity(0.4)], startPoint: UnitPoint.top, endPoint: UnitPoint.bottom))
+                    VStack {
+                        Text(city.name).font(.largeTitle).bold()
+                        Text(city.tagline).font(.title2)
+                    }
+                    .foregroundStyle(.white)
+                    .padding()
                 }
-                Rectangle()
-                    .fill(.linearGradient(colors: [Color.clear, Color.black.opacity(0.4)], startPoint: UnitPoint.top, endPoint: UnitPoint.bottom))
-                VStack {
-                    Text(city.name).font(.largeTitle).bold()
-                    Text(city.tagline).font(.title2)
-                }
-                .foregroundStyle(.white)
-                .padding()
-            }
-            .frame(height: 200)
+                .frame(height: 200)
 
-            List {
-                CountryInfoSection()
-                Section("Related Destinations") {
-                    ForEach(relatedCities) { city in
-                        NavigationLink(value: city.id) {
-                            HStack {
-                                AsyncImage(url: city.imageURL) { image in
-                                    image.resizable().clipped()
-                                } placeholder: {
-                                    ProgressView()
-                                }
-                                .cornerRadius(8)
-                                .frame(width: 50, height: 50)
-                                VStack(alignment: .leading) {
-                                    Text(city.name)
-                                    Text(city.tagline)
-                                }
-                            }
-                        }
+                List {
+                    Section {
+                        CountryInfoSection()
+                    }
+                    Section("Related Destinations") {
+                        ForEach(relatedCities, content: cityNavigationLink)
                     }
                 }
             }
+            .toolbar(.hidden, for: .navigationBar)
+            .ignoresSafeArea(edges: .top)
         }
-        .ignoresSafeArea(edges: .top)
+    }
+
+
+    fileprivate func cityNavigationLink(_ city: City) -> some View {
+        NavigationLink(value: city.id) {
+            HStack {
+                AsyncImage(url: city.imageURL) { image in
+                    image.resizable().clipped()
+                } placeholder: {
+                    ProgressView()
+                }
+                .cornerRadius(8)
+                .frame(width: 50, height: 50)
+                VStack(alignment: .leading) {
+                    Text(city.name)
+                    Text(city.tagline)
+                }
+            }
+        }
     }
 
 
     fileprivate func CountryInfoSection() -> some View {
-        Section {
+        Group {
             HStack {
                 Text("Country")
                 Spacer()
